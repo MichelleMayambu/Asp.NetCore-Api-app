8f6627557ed29ed82723926f9c1c96b654002d92
MICHELLE MAYAMBU <michelle@netvendor.co.za>
2020-05-14 14:30:19 +0200

    Add quotesDbContext instance to QuotesController and set Get() to return Quote object from Context

---
+++ Asp.NetCore Api app/Controllers/QuotesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Asp.NetCore_Api_app.Data;
+using Asp.NetCore_Api_app.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore_Api_app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        //get quotes from dataBase create instance of quotes db context class
+        private QuotesDbContext _quotesDbContext = new QuotesDbContext();
+
+        // GET: api/Quotes
+        [HttpGet]
+        public IEnumerable<Quote> Get()
+        {
+            return _quotesDbContext.Quotes;
+        }
+
+        // GET: api/Quotes/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Quotes
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Quotes/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
