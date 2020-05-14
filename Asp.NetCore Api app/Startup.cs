d9c00fac27628af08f45ba098a6b96f39dec190a
MICHELLE MAYAMBU <michelle@netvendor.co.za>
2020-05-14 19:28:03 +0200

    Set up Connection string in AppSettings.json file

--- Asp.NetCore Api app/Startup.cs
+++ Asp.NetCore Api app/Startup.cs
@@ -27,6 +27,9 @@ namespace Asp.NetCore_Api_app
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //register database context service with dependency injection
+          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
